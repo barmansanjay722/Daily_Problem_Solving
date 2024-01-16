93d8cfc428c74b916a87bddd460ef11d11587fe8
sanjay <barmansanjay722@gmail.com>
2024-01-16 22:56:35 +0530

    fist commit

---
+++ src/main/java/org/DSA/demo2/live_problems/Hello.java
@@ -0,0 +1,7 @@
+package org.DSA.demo2.live_problems;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/main/java/org/DSA/demo2/video_problems/Hello.java
@@ -0,0 +1,7 @@
+package org.DSA.demo2.video_problems;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
