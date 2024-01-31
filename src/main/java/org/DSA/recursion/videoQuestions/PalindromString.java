b7357f6553d001fe7a6ee7e2707eb56e532508ae
sanjay <barmansanjay722@gmail.com>
2024-02-01 00:03:43 +0530

    Count_OccurenceString and PringSubSets

--- src/main/java/org/DSA/recursion/videoQuestions/PalindromString.java
+++ src/main/java/org/DSA/recursion/videoQuestions/PalindromString.java
@@ -6,6 +6,17 @@ public class PalindromString {
     }
 
     static boolean isPalindrom(String s) {
+        return isPalindromHelper(s,0,s.length()-1);
+    }
 
+    static boolean isPalindromHelper(String s, int l, int r) {
+        if(l >= r) return true;
+
+        if(s.charAt(l) == s.charAt(r)) {
+            return isPalindromHelper(s, l+1, r-1);
+        }
+        else {
+            return false;
+        }
     }
 }
