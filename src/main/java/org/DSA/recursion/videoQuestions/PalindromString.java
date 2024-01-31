6347cb4403a442efb2dcef4d1cec38af502c98c9
barmansanjay722 <barmansanjay722@gmail.com>
2024-01-31 17:50:18 +0530

    code refractor

---
+++ src/main/java/org/DSA/recursion/videoQuestions/PalindromString.java
@@ -0,0 +1,11 @@
+package org.DSA.recursion.videoQuestions;
+
+public class PalindromString {
+    public static void main(String[] args) {
+        System.out.println(isPalindrom("racecar"));
+    }
+
+    static boolean isPalindrom(String s) {
+
+    }
+}
