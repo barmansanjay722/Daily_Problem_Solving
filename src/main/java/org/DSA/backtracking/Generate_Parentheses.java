eea0886d8e111afc313bde027fa54937f923628b
barmansanjay722 <barmansanjay722@gmail.com>
2024-02-08 17:53:54 +0530

    permutaion of array and rate in maze

---
+++ src/main/java/org/DSA/backtracking/Generate_Parentheses.java
@@ -0,0 +1,7 @@
+package org.DSA.backtracking;
+
+public class Generate_Parentheses {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/DSA/backtracking/Perutation_of_Array.java
@@ -0,0 +1,38 @@
+package org.DSA.backtracking;
+
+
+// important question like google, microsoft
+
+public class Perutation_of_Array {
+    public static void main(String[] args) {
+
+        int a[] = {1,2,3};
+        permute(a,0,a.length-1);
+    }
+
+    static void permute(int a[], int l , int r) {
+        if(l == r) {
+            printArray(a);
+            return;
+        }
+
+        for(int i=l;i<=r;i++) {
+            swap(a,i,l);
+            permute(a, l+1,r);
+            swap(a,i,l);   // Backtracking
+        }
+    }
+
+    private static void swap(int a[], int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    private static void printArray(int a[]) {
+        for(int e: a) {
+            System.out.print(e);
+        }
+        System.out.println();
+    }
+}
