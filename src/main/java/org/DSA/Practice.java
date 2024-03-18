2c7d7bb0762d8a9d9cdd49f651de52a70315e7fa
sanjay <barmansanjay722@gmail.com>
2024-03-19 00:19:13 +0530

    code refractor

--- src/main/java/org/DSA/Practice.java
+++ src/main/java/org/DSA/Practice.java
@@ -6,51 +6,42 @@ public class Practice {
     public static void main(String[] args) {
         Node head = new Node(1);
         Node n1 = new Node(2);
-        Node n2 = new Node(3);
-        Node n3 = new Node(4);
-        Node n4 = new Node(5);
-        Node n5 = new Node(6);
+        Node n2 = new Node(6);
+        Node n3 = new Node(3);
+        Node n4 = new Node(4);
+        Node n5 = new Node(5);
+        Node n6 = new Node(6);
 
-        head.next = n1; n1.next = n2; n2.next = n3; n3.next = n4; n4.next = n5;
+        head.next = n1; n1.next = n2; n2.next = n3; n3.next = n4; n4.next = n5;n5.next = n6;
         printLL(head);
-//        Node revLL = reverseLL(head);
-//        printLL(revLL);
-        reverseKGroup(head,3);
+        Node res = removeElements(head, 6);
+        printLL(res);
     }
 
-    static void reverseKGroup(Node head, int k) {
+    static Node removeElements(Node head, int val) {
+
+        if(head == null || head.next == null) return null;
+
         Node temp = head;
-        Node ans = new Node(-1);
-        int count = 0;
-        while(count != 3) {
-            System.out.print(temp.val + " -> ");
-            ans.next = temp;
+        Node newNode = null;
+
+        while(temp != null) {
+            if(temp.val == val) {
+                newNode = temp = head.next;
+            }
+            else {
+                newNode = temp = head;
+                newNode = newNode.next;
+            }
             temp = temp.next;
-            count++;
         }
-    }
-
-    static Node reverseLL(Node head) {
-        if(head == null || head.next == null) return head;
-
-        Node prev = head;
-        Node cur = head.next;
-        head.next = null;
-
-        while (cur != null) {
-            Node next = cur.next;
-            cur.next = prev;
-            prev = cur;
-            cur = next;
-        }
-
-        return prev;
+        return newNode;
     }
 
 
     static void printLL(Node head) {
-        while (head != null) {
-            System.out.print(head.val + " -> ");
+        while(head != null) {
+            System.out.print(head.val+" -> ");
             head = head.next;
         }
         System.out.println();
